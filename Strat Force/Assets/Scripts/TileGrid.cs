003359e6930ea60ef5f8db3c0df092228edb423f
Joseph <jpraposo2027@gmail.com>
2025-09-17 20:31:16 -0400

    addedtwo script gridmanager grid tiile

--- Strat Force/Assets/Scripts/TileGrid.cs
+++ Strat Force/Assets/Scripts/TileGrid.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TileGrid : MonoBehaviour
 {
-    public GameObject[] tiles;
-
-    public string TileTag = "Tile";
+    List<List<GameObject>> grid = new List<List<GameObject>>();
+    public int width = 10;
+    public int hight = 10;
+    public float tileSize = 2;
+    public GameObject tilePrefab; 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        tiles = GameObject.FindGameObjectsWithTag(TileTag);
+        for(int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < hight; j++)
+            {
+
+            }
+
+
+        }
     }
 
     // Update is called once per frame
